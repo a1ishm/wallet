f1a2954e6b0aedfd4caa63b9ca592380675dedc3
a1ishm <alishermajidov19@gmail.com>
2021-04-17 01:18:24 +0500

    exporttofile func added

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"github.com/a1ishm/wallet/pkg/types"
+	"log"
+	"os"
+	"strconv"
+)
 
-	"github.com/a1ishm/wallet/pkg/wallet")
 
 
 func main() {
-	svc := &wallet.Service{}
-	account, err := svc.RegisterAccount("+992000000001")
-	if err != nil {
-		fmt.Println(err)
-		return
+	accounts := []*types.Account{
+		{ID: 1, Phone: "+992000000001", Balance: 10_000_00},
+		{ID: 2, Phone: "+992000000002", Balance: 20_000_00},
+		{ID: 3, Phone: "+992000000003", Balance: 30_000_00},
 	}
 
-	err = svc.Deposit(account.ID, 10)
+	path := "files/accs.txt"
+
+	file, _ := os.Create(path)
+	defer func() {
+		cerr := file.Close()
+		if cerr != nil {
+			log.Print(cerr)
+		}
+	}()
+	
+	var content string
+
+	for i, account := range accounts {
+		id := strconv.FormatInt(account.ID, 10)
+		phone := string(account.Phone)
+		balance := strconv.FormatInt(int64(account.Balance), 10)
+
+		var acc string
+
+		if i == (len(accounts) - 1) {
+			acc = id + ";" + phone + ";" + balance 
+		} else {
+			acc = id + ";" + phone + ";" + balance + "|"
+		}
+		
+		content += acc
+	}
+	
+	_, err := file.Write([]byte(content))
+
 	if err != nil {
-		fmt.Println(err)
+		log.Print(err)
 		return
 	}
-
-	fmt.Println(account.Balance)
 }
